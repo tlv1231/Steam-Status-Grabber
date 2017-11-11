131c72fc3ce7f9209b25f73f62899411ea4dfb0f
tlv1231 <33561172+tlv1231@users.noreply.github.com>
2017-11-10 23:51:24 -0500

    Update steamstatusgrabber.py

--- steamstatusgrabber.py
+++ steamstatusgrabber.py
@@ -1,17 +1,17 @@
 import urllib2
 from datetime import datetime
 from bs4 import BeautifulSoup
-quote_page = 'http://steamcommunity.com/profiles/XXXXXXXXXXXXXX'
+quote_page = 'http://steamcommunity.com/id/XXXXXXXXXXXXXXXXXXXX/'
 # above url must be replaced with a public steam user's profile
 page = urllib2.urlopen(quote_page)
 soup = BeautifulSoup(page, 'html.parser')
-price_box = soup.find('div', attrs={'class':'profile_in_game_header'})
-price = price_box.text
-print price
+status_box = soup.find('div', attrs={'class':'profile_in_game_header'})
+status = status_box.text
+print status
 print str(datetime.now())
 
 
 import csv
 with open('index.csv', 'a') as csv_file:
 	writer = csv.writer(csv_file)
-	writer.writerow([price, datetime.now()])
+	writer.writerow([status, datetime.now()])
